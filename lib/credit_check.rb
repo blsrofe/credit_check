cc8ce8e97b3bc0d914dff0af60f6ac3634d75711
Becki Srofe <blf_23@yahoo.com>
2017-06-28 15:11:24 -0600

    refactor sum_digits

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,27 +1,27 @@
-
-#create method to add digits together for third line
 def sum_digits(num)
-  num.to_s.split("").inject(0) {|sum, n| sum + n.to_i}
+  single_digit = num.to_s.split("")
+  single_digit.inject(0) do |sum, n|
+    sum + n.to_i
+  end
 end
 
-#get credit card number
 puts "What is the card number?"
 card_number = gets.chomp
-#put card number into array
+
 first_line = card_number.split(//)
-#iterate through array and create second line array
+
+#def create_second_line
+#end
+
 second_line = []
 first_line.each_with_index do |number, index|
   if index % 2 == 0
-    #multiply by number by 2 and send to second_line array
     second_line.push(number.to_i * 2)
   else
-    #send original number to seond_line array
     second_line.push(number.to_i)
   end
 end
 
-#make third line array
 third_line = second_line.map do |num|
   if num.to_s.length >= 2
     sum_digits(num)
@@ -29,13 +29,14 @@ third_line = second_line.map do |num|
     num
   end
 end
-#add all third line values together
+
 sum = 0
-final_sum = third_line.each {|digit| sum += digit}
-#if all third line values % 10 == 0 then puts "The number is valid!"
+final_sum = third_line.each do |digit|
+  sum += digit
+end
+
 if sum % 10 == 0
   puts "The number is valid!"
-#if all third line values % 10 != 0 then puts "The number is not valid!"
 else
   puts "The number is not valid!"
 end
