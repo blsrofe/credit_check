3194cb351c61099ad9f15f13e6610dd2c5b04c47
Becki Srofe <blf_23@yahoo.com>
2017-06-27 20:10:18 -0600

    finish hash version_has error

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,6 +1,7 @@
-#create doubler method
-def doubler (num)
-  num_to.i * 2
+
+#create method to add digits together for third line
+def sum_digits(num)
+  num.to_s.split("").inject(0) {|sum, n| sum + n.to_i}
 end
 
 #get credit card number
@@ -10,6 +11,7 @@ card_number = gets.chomp
 split_card_number = card_number.split(//)
 #put array into a hash with indexes
 indexes = Hash[split_card_number.map.with_index.to_a]
+#problem in hash, does not work when digits are the same in number
 #create bland second line array and fill with doubled numbers
 second_line = []
 indexes.each do |number, index|
@@ -21,8 +23,22 @@ indexes.each do |number, index|
     second_line.push(number.to_i)
   end
 end
-puts second_line
 #make third line array
+third_line = second_line.map do |num|
+  if num.to_s.length >= 2
+    sum_digits(num)
+  else
+    num
+  end
+end
 #add all third line values together
+sum = 0
+final_sum = third_line.each {|digit| sum += digit}
+puts sum
 #if all third line values % 10 == 0 then puts "The number is valid!"
+if sum % 10 == 0
+  puts "The number is valid!"
 #if all third line values % 10 != 0 then puts "The number is not valid!"
+else
+  puts "The number is not valid!"
+end
