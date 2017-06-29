ecb091222c8cc4c6a3126aa7fa0543b92dbda235
Becki Srofe <blf_23@yahoo.com>
2017-06-28 22:02:17 -0600

    added methods and ae

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,8 +1,54 @@
+require "pry"
+def double_second_number_even(card_number_split)
+  doubled_collection = []
+  card_number_split.each_with_index do |number, index|
+    if index % 2 == 0
+      doubled_collection.push(number.to_i * 2)
+    else
+      doubled_collection.push(number.to_i)
+    end
+  end
+  sum_multi_digit_nums(doubled_collection)
+end
+
+def double_second_number_odd(card_number_split)
+  doubled_collection = []
+  card_number_split.each_with_index do |number, index|
+    if index % 2 != 0
+      doubled_collection.push(number.to_i * 2)
+    else
+      doubled_collection.push(number.to_i)
+    end
+  end
+  sum_multi_digit_nums(doubled_collection)
+end
+
+def sum_multi_digit_nums(doubled_collection)
+  summed_collection = []
+  doubled_collection.each do |num|
+    if num.to_s.length >= 2
+      summed_collection.push(sum_digits(num))
+    else
+      summed_collection.push(num)
+    end
+  end
+  sum_all_nums(summed_collection)
+end
+
 def sum_digits(num)
   single_digit = num.to_s.split("")
   single_digit.inject(0) do |sum, n|
     sum + n.to_i
   end
+end
+
+def sum_all_nums(summed_collection)
+  sum = 0
+  summed_collection.each do |digit|
+    sum += digit
+  end
+  sum
+  print_valid_invalid_statement(sum)
 end
 
 def print_valid_invalid_statement(sum)
@@ -16,28 +62,10 @@ end
 puts "What is the card number?"
 card_number = gets.chomp
 
-first_line = card_number.split(//)
+card_number_split = card_number.split(//)
 
-second_line = []
-first_line.each_with_index do |number, index|
-  if index % 2 == 0
-    second_line.push(number.to_i * 2)
-  else
-    second_line.push(number.to_i)
-  end
+if card_number_split.count % 2 == 0
+  double_second_number_even(card_number_split)
+else
+  double_second_number_odd(card_number_split)
 end
-
-third_line = second_line.map do |num|
-  if num.to_s.length >= 2
-    sum_digits(num)
-  else
-    num
-  end
-end
-
-sum = 0
-third_line.each do |digit|
-  sum += digit
-end
-
-print_valid_invalid_statement(sum)
